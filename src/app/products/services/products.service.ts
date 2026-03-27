6e3f968c6639d2cf5acf3c08215eada84e5978bb
Mario Toj <etalvarez03@gmail.com>
2026-03-27 16:08:01 -0600

    creating auth feature

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '../interfaces/products-response.interface';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -17,20 +17,42 @@ interface Options {
 })
 export class ProductsService {
   private http = inject(HttpClient);
+  // Cache
+  private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+    const keyProducts = `${limit}-${offset}-${gender}`;
 
-    return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
-      params: {
-        limit,
-        offset,
-        gender,
-      },
-    });
+    if (this.productsCache.has(keyProducts)) {
+      return of(this.productsCache.get(keyProducts)!);
+    }
+
+    return this.http
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit,
+          offset,
+          gender,
+        },
+      })
+      .pipe(
+        // tap((resp) => console.log(resp)),
+        tap((resp) => this.productsCache.set(keyProducts, resp)),
+      );
   }
 
   getProductById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${id}`);
+    const keyProduct = `${id}`;
+
+    if (this.productCache.has(keyProduct)) {
+      return of(this.productCache.get(keyProduct)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${id}`).pipe(
+      // tap((resp) => console.log(resp)),
+      tap((resp) => this.productCache.set(keyProduct, resp)),
+    );
   }
 }
